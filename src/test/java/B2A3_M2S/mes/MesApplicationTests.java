9c1792772d71c7eb600a597726fdd9794be8f51e
지민 <81950388+jjm6604@users.noreply.github.com>
2023-05-01 16:14:21 +0900

    1

---
+++ src/test/java/B2A3_M2S/mes/MesApplicationTests.java
@@ -0,0 +1,13 @@
+package B2A3_M2S.mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

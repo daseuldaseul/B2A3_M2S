472b3a6dd4404a08fc3ad046c8dce448a5f26386
ckddms6530 <ckddms6530@gmail.com>
2023-05-23 09:13:37 +0900

    123

--- src/main/java/B2A3_M2S/mes/controller/ObtainOrderController.java
+++ src/main/java/B2A3_M2S/mes/controller/ObtainOrderController.java
@@ -19,6 +19,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -41,17 +42,6 @@ public class ObtainOrderController {
     @Autowired
     ItemRepository itemRepository;
 
-    @Autowired
-    CompanyRepository companyRepository;
-
-    @Autowired
-    ObtainOrderRepository obtainOrderRepository;
-
-    @Autowired
-    ItemRepository itemRepository;
-
-    @Autowired
-    ObtainOrderService obtainOrderService;
 
     @GetMapping("/obtainOrder")
     public String ObtainOrder(Model model) {

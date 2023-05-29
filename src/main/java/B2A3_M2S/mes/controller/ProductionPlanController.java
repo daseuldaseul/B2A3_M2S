e5b895bd24ed5d590e2f7963e7a5ab1a5454b337
박다슬 <121841821+daseuldaseul@users.noreply.github.com>
2023-05-29 23:51:51 +0900

    ajax시간설정때문에미쳐부러

--- src/main/java/B2A3_M2S/mes/controller/ProductionPlanController.java
+++ src/main/java/B2A3_M2S/mes/controller/ProductionPlanController.java
@@ -1,16 +1,23 @@
 package B2A3_M2S.mes.controller;
 
 import B2A3_M2S.mes.dto.ProductionDTO;
+import B2A3_M2S.mes.dto.RoutingDto;
+import B2A3_M2S.mes.entity.Item;
 import B2A3_M2S.mes.entity.ObtainOrder;
 import B2A3_M2S.mes.entity.Production;
+import B2A3_M2S.mes.entity.Routing;
 import B2A3_M2S.mes.repository.ObtainOrderRepository;
 import B2A3_M2S.mes.repository.ProductionRepository;
 import B2A3_M2S.mes.service.CodeServiceImpl;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -24,23 +31,44 @@ public class ProductionPlanController {
     @GetMapping("/productionPlan")
     public String productionPlan(Model model){
         List<ObtainOrder> obtainOrderList = obtainOrderRepository.findAll();
-        List<Production> productionPlan = productionRepository.findAll();
-        List<ProductionDTO> productionDtoList = ProductionDTO.of(productionPlan);
+        List<ProductionDTO> productionDtoList = new ArrayList<>();
 
-        if(!productionDtoList.isEmpty()) {
-            ProductionDTO firstProductionDto = productionDtoList.get(0);
-            ProductionDTO lastProductionDto = productionDtoList.get(productionDtoList.size() - 1);
-
-            // 가져온 객체를 모델에 추가
-            model.addAttribute("firstProductionDto", firstProductionDto);
-            model.addAttribute("lastProductionDto", lastProductionDto);
-
+        for(ObtainOrder obtainOrder : obtainOrderList) {
+            List<Production> productionPlan = productionRepository.findByObtainOrderOrderCd(obtainOrder.getOrderCd());
+            productionDtoList.addAll(ProductionDTO.of(productionPlan));
         }
 
-            model.addAttribute("productionList", productionDtoList);
 
+//        if (!productionDtoList.isEmpty()) {
+//            List<ProductionDTO> firstProductionDto = new ArrayList<>();
+//            firstProductionDto.add(productionDtoList.get(0));
+//
+//            List<ProductionDTO> lastProductionDto = new ArrayList<>();
+//            lastProductionDto.add(productionDtoList.get(productionDtoList.size() - 1));
+//
+//            model.addAttribute("firstProductionDto", firstProductionDto);
+//            model.addAttribute("lastProductionDto", lastProductionDto);
+//        }
+
+        model.addAttribute("productionList", productionDtoList);
         model.addAttribute("obtainOrderList", obtainOrderList);
         return "productionPlanPage";
     }
 
+
+
+    @ResponseBody
+    @GetMapping("/production/detail")
+    public String productionDetail(@RequestParam String orderCd, Model model) {
+
+        Gson gson = new Gson();
+        ObtainOrder obtainOrder = obtainOrderRepository.findSingleByOrderCd(orderCd);
+        List<Production> production = productionRepository.findByObtainOrder(obtainOrder);
+        List<ProductionDTO> productionDTOList = ProductionDTO.of(production);
+
+
+
+        String json = gson.toJson(productionDTOList);
+        return json;
+    }
 }
--- src/main/java/B2A3_M2S/mes/repository/ObtainOrderRepository.java
+++ src/main/java/B2A3_M2S/mes/repository/ObtainOrderRepository.java
@@ -13,6 +13,7 @@ public interface ObtainOrderRepository extends JpaRepository<ObtainOrder, String
     List<ObtainOrder> findByOrderCd(String orderCd);
 
 
+    ObtainOrder findSingleByOrderCd(String orderCd);
 
 
 
